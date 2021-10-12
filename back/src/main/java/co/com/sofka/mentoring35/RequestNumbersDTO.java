a3a417561ffa76e992c5749816a935819dfe67ab
Diego Martinez <juandiego337@hotmail.com>
2021-10-12 18:15:26 -0500

    Reto mentoring 35 terminado

---
+++ back/src/main/java/co/com/sofka/mentoring35/RequestNumbersDTO.java
@@ -0,0 +1,33 @@
+package co.com.sofka.mentoring35;
+
+public class RequestNumbersDTO {
+
+  private String list;
+  private int number1;
+  private int number2;
+
+  public String getList() {
+    return list;
+  }
+
+  public void setList(String list) {
+    this.list = list;
+  }
+
+  public int getNumber1() {
+    return number1;
+  }
+
+  public int getNumber2() {
+    return number2;
+  }
+
+  public void setNumber2(int number2) {
+    this.number2 = number2;
+  }
+
+  public void setNumber1(int number1) {
+    this.number1 = number1;
+  }
+
+}
---
+++ front/src/actions/index.js
@@ -0,0 +1,38 @@
+
+
+export const fetchRandom = (state) => (dispatch) => {
+
+    dispatch({ type: "view-loading" });
+
+    return fetch(`http://localhost:8080/r`, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({list: state}) // body data type must match "Content-Type" header
+    }).then(response => response.json())
+      .then(json => {
+          dispatch({ type: "random-result", data: json });
+          dispatch({ type: "view-loaded" });
+        })
+}
+
+export const fetchRandomNumber = (state) => (dispatch) => {
+
+  dispatch({ type: "view-loading" });
+
+  return fetch(`http://localhost:8080/r/n`, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({number1: state.number1, number2: state.number2}) // body data type must match "Content-Type" header
+  }).then(response => response.json())
+    .then(json => {
+        dispatch({ type: "random-result", data: json });
+        dispatch({ type: "view-loaded" });
+      })
+}
+
+
+//TODO: agregar las demas acciones
